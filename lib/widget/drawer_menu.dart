9c65bebb3eb758e3c39a4c7677ad4e341732e7af
SC Kiyotaka Takizuka <takizuka_kiyotaka@sharp.co.jp>
2020-02-15 01:05:30 +0900

    iOSでSafe Areaによりドロワー上部に空白ができる問題の修正

--- lib/widget/drawer_menu.dart
+++ lib/widget/drawer_menu.dart
@@ -6,6 +6,8 @@ class DrawerMenu extends StatelessWidget {
   build(BuildContext context) {
     return new Drawer(
       child: ListView(
+        // iOS X padding fix: https://github.com/flutter/flutter/issues/22190
+        padding: const EdgeInsets.all(0.0),
         children: <Widget>[
           new UserAccountsDrawerHeader(
             accountName: new Text('お問い合わせは以下まで'),
